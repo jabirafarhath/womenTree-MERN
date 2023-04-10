55ae53c399fe4ac9d9d39513b0d37f5a5ac1bdea
Jabira Farhath M A <jabirafarhath.mec@gmail.com>
2023-04-10 23:18:10 +0530

    modified auth routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const connection = require("../config/db");
-const { connect } = require("mongoose");
 
 //login with google
 router.get(
