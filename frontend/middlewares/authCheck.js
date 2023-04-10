817211ac8e976f57e189fe91cd575a3d46197322
Jabira Farhath M A <jabirafarhath.mec@gmail.com>
2023-04-10 23:11:40 +0530

    modified useAuthCheck function

--- frontend/middlewares/authCheck.js
+++ frontend/middlewares/authCheck.js
@@ -4,16 +4,16 @@ import axios from "axios";
 export function useAuthCheck() {
   const [isAuthenticated, setAuth] = useState(false);
   const [error, setError] = useState(false);
-  const [userId, setUser] = useState(false);
+  const [userId, setUserId] = useState(false);
   useEffect(() => {
     axios
       .get("http://localhost:3001/auth/session", {
         withCredentials: true,
       })
       .then((response) => {
-        // console.log(response);
+        // console.log(response.data);
         setAuth(true);
-        setUser(response);
+        setUserId(response.data);
       })
       .catch((err) => {
         setError(err.response.data);
